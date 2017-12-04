ef9ceacd8917b73416fbe990c72cb57675500487
Derrck Persson <derrickpersson@gmail.com>
2017-12-04 14:26:04 -0800

    Add options object variable to main function

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,16 @@
 var request = require('request');
 
 function getRepoContributors(repoOwner, repoName, cb){
+  var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent' : 'request'
+      }
+  }
 
+  request(url, function(err, response, body){
+    cb(err, body);
+  })
 };
 
 getRepoContributors("jquery", "jquery", function(err, result){
