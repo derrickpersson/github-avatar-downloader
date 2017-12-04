599fa0b3e38f1993484ba77f437c00a9c25efd75
Derrck Persson <derrickpersson@gmail.com>
2017-12-04 14:44:06 -0800

    Add authorization to request call

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,19 @@
 var request = require('request');
+var token = require('secrets.js');
 
 function getRepoContributors(repoOwner, repoName, cb){
   var options = {
     url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent' : 'request'
+      'Authorization' : 'token '
       }
   }
 
   request(url, function(err, response, body){
     cb(err, body);
   })
+  request('Authorization')
 };
 
 getRepoContributors("jquery", "jquery", function(err, result){
