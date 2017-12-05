4d92e99941e2809b0e51b3367c8a42355267c10b
Derrck Persson <derrickpersson@gmail.com>
2017-12-04 16:30:48 -0800

    Authorization works

--- download_avatars.js
+++ download_avatars.js
@@ -1,19 +1,20 @@
 var request = require('request');
-var token = require('secrets.js');
+var token = require('./secrets.js');
 
 function getRepoContributors(repoOwner, repoName, cb){
   var options = {
     url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent' : 'request'
-      'Authorization' : 'token '
-      }
+      },
+    Authorization : token,
+    json : true
   }
 
-  request(url, function(err, response, body){
+  request(options, function(err, response, body){
+    console.log(body);
     cb(err, body);
   })
-  request('Authorization')
 };
 
 getRepoContributors("jquery", "jquery", function(err, result){
